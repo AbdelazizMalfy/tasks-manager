93b7dfa32ff7dee111eedebcb17c81e8e43b2517
Abdelaziz <abdelazizMalfy@gmail.com>
2019-11-24 10:51:52 +0100

    unique username validation added

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -1,6 +1,7 @@
 import { Repository, EntityRepository } from "typeorm";
 import { User } from "./auth.entity";
 import { AuthCredentialsDto } from "./dto/Auth-credentials.dto";
+import { ConflictException, BadRequestException, InternalServerErrorException } from "@nestjs/common";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User>{
@@ -11,6 +12,15 @@ export class UserRepository extends Repository<User>{
         const user = new User();
         user.username = username;
         user.password = password;
-        await user.save()
+
+        try{
+            await user.save()
+        }catch (error) {
+            if(error.code ='23505'){
+                throw new ConflictException('Username already exists')
+            }else {
+                throw new InternalServerErrorException();
+            }
+        }
     }
 }
